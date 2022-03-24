032daba3c3417720929059b62c95033504d4a1c5
Nikita <KROLLIKauto94@yandex.ru>
2022-03-24 14:56:35 +0300

    redux

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; 
+import App from './App';
+ 
+import { Provider } from 'react-redux';
+import { store } from '../src/Redux/store';
+
+
+
+window.store = store
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  
+  document.getElementById('root')
+  );
